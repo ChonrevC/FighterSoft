b5b4f09ea3636095996b0ea52ccbaa639584b086
ChonrevC <chonrev.cal@mavs.uta.edu>
2022-04-01 11:47:36 -0500

    declare components of settings activity

--- app/src/main/java/com/example/fightersoft/Settings.java
+++ app/src/main/java/com/example/fightersoft/Settings.java
@@ -3,8 +3,17 @@ package com.example.fightersoft;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class Settings extends AppCompatActivity {
+
+    // declare the components in the activity
+    TextInputEditText textInputEditTextUsername, textInputEditTextPassword;
+    TextView textViewSettings;
+    Button updateButton, colorButton, deleteButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
